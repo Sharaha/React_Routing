a595ca168b36533d06df25001393cd6c80718a91
Sharaha <sharahasri@gmail.com>
2021-05-14 12:44:00 +0530

    Commit successful

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Route, BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Customer from './Customer';
+import Product from './Product';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const routing=(
+  <Router>
+    <div>
+      <Route exact path="/" component ={App} />
+      <Route path="/Customer" component={Customer} />
+      <Route path="/Product" component={Product}  />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing,document.getElementById('root'))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
